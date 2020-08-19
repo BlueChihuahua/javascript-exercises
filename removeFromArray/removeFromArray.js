f383cc10398712ad58762b81562eb0679c6302fe
BlueChihuahua <mmunk1994@gmail.com>
2020-08-18 21:18:21 -0600

    updating solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let arr = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    if (!(arr.some(element => element === arguments[i]))) {
+        continue;
+    } else {
+    let index = arr.findIndex((element) => element === arguments[i]);
+    arr.splice(index, 1);
+    }
+  }
+  return arr;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
