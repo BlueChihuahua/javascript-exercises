5bdb9a306241075e66508a9490dd684ed1761df0
BlueChihuahua <mmunk1994@gmail.com>
2020-08-18 22:02:54 -0600

    update answers

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let begin = a < b ? a : b;
+  let end = b > a ? b : a;
+  if (typeof begin !== "number" || typeof end !== "number") {
+    return "ERROR";
+  } else if (begin < 0 || end < 0) {
+    return "ERROR";
+  } else {
+    for (let i = begin; i < end; i++) {
+      sum += i;
+    }
+    return sum + end;
+  }
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
