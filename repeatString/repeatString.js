0f6f7b13e5faead5306da5fbc8b4d9c5b380e200
BlueChihuahua <mmunk1994@gmail.com>
2020-08-18 20:03:57 -0600

    update solutions

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let result = '';
+    for (let i = 0; i < num; i++) {
+      result += string;
+    }
+    return result;
+  } else return 'ERROR';
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = Array.from(string);
+    console.log(arr);
+    let reversed = arr.reverse();
+    return reversed.join('');
 }
 
 module.exports = reverseString
