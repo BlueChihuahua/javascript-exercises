d76d5234d4761f3ec5d75330954780eaf0319d0b
BlueChihuahua <mmunk1994@gmail.com>
2020-08-18 23:23:06 -0600

    update answers

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array1) {
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+	return array1.reduce(reducer, 0);
 }
 
-function multiply () {
-	
+function multiply (args) {
+	let total = 1;
+	args.forEach(element => {
+		total *= element;
+		
+	});
+	return total;
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let total = 1;
+	if (a == 0) {
+		return 1;
+	} else {
+		for (i = a; i > 0; i--) {
+			total *= i;
+		}
+	}
+	return total;
 }
 
 module.exports = {
